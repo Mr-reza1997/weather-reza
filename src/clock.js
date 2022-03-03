e4269e42f9d2bcf0e21973a34e86238206d38b6c
Reza Amiri <Rezaamiri842@gmail.com>
2022-03-03 23:25:06 +0330

    weather

---
+++ src/clock.js
@@ -0,0 +1,21 @@
+// import React from 'react'
+// import reactDom from 'react-dom';
+
+
+
+
+
+// export let tick = () =>{
+//   const element = (
+//       <div>
+//           <h2>Time Is {new Date().toLocaleTimeString()}</h2>
+//       </div>
+//   )
+
+//   reactDom.render(
+//       element,document.getElementById('root')
+  
+//       );
+// }
+
+// setInterval(tick, 1000);
